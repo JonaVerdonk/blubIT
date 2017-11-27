a26a3578b2be9250d385bec6fdcc645ff304236c
JonaVerdonk <32864379+JonaVerdonk@users.noreply.github.com>
2017-11-27 11:03:10 +0100

    Commit test

--- php/admin.php
+++ php/admin.php
@@ -23,6 +23,7 @@
         <div id="pageContent">
 
             <div id="adminLinks">
+                Dit is een github test. ignore.
                 <div class="adminItem">
                     <a href="../php/analytics.php">
                         <img src="../images/350x150.png">
