7c7ec554b7bab03dd7d0d9e923eb02a6110032ea
LordPinch <Dennis.1000@live.nl>
2017-11-28 12:55:34 +0100

    CMS update (Images)
    cms supports now images and footer may be fixed

--- scripts/content.php
+++ scripts/content.php
@@ -2,17 +2,26 @@
 include("databaseConnection.php.php");
 $contentAdmin = true;
 
-$array = array(1,1,1,1,1,1,11,1,2,3,5,6,3,4,5,6,7,8,3,3,121,1);//Get list of content in page
+$array = array(1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3);//Get list of content in page
 
-echo "<link rel='stylesheet' type='text/css' href='css/content.css'> <div id='content'>";
+echo "<link rel='stylesheet' type='text/css' href='css/content.css'> <div id='content'>"; //Add the styling
+  //Cycle through the array
 foreach ($array as $index => $ContentID) {
+    //Create a div
   echo "<div class='content-body'>";
+    //Get all the textfields in the contentBox
   $textFields = executeSQL("SELECT content FROM Text WHERE contentID = $ContentID");
+    //Cycle and print all text
   foreach ($textFields as $key => $value) {
     echo "<p>" . $value[0] . "</p>";
   }
+    //Get all the images in the contentBox
+    $ImageFields = executeSQL("SELECT width,height,alt,url FROM Image WHERE contentID = $ContentID");
+
+    foreach ($ImageFields as $key => $value) {
+      echo "<img style='width: $value[0]%;height: $value[1]px' alt='$value[2]' src='$value[3]'>";
+    }
   echo"</div>";
 }
 echo "</div>";
-
  ?>
