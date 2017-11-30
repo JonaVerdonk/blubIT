0428c1d857b9a574ebbb916529f9160c02e485bf
JonaVerdonk <32864379+JonaVerdonk@users.noreply.github.com>
2017-11-30 14:16:59 +0100

    cms Navbar zooi

--- php/CMS/cmsNavbar.php
+++ php/CMS/cmsNavbar.php
@@ -8,7 +8,7 @@
     </head>
     <body>
 
-        <?php //include($_SERVER['DOCUMENT_ROOT']."scripts/header.php"); ?>
+        <?php include($_SERVER['DOCUMENT_ROOT']."scripts/header.php"); ?>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109575524-1"></script>
@@ -35,7 +35,7 @@
                         url: "../../scripts/executeQuery.php",
                         type: "POST",
                         data: {"sql": "SELECT * FROM Navbar ORDER BY position;"},
-                        success: function(data, status) {
+                        success: function(data, status) {*/
                               //De beste manier om het woord array te spellen
 //                            for (i = 0; i < data.length; ++ i) {
 //                               $("#navbarTable").append("<tr><td>"+data[i][2]+"</td><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td></tr>");
@@ -43,9 +43,10 @@
 
                             var json = data;
                             var data = $.parseJSON(json);
+
                             $("#navbarTable").append("<tr><th>Order</th><th>URL</th><th>Name</th></tr>");
                             for (i = 0; i < data.length; ++ i) {
-                                $("#navbarTable").append("<tr><td class='position'>"+data[i][2]+"</td><td class='url'>"+data[i][0]+"</td><td class='name'>"+data[i][1]+"</td><td><button class='edit' value='edit'></td></tr>");
+                                $("#navbarTable").append("<tr><td class='position'>"+data[i][2]+"</td><td class='url'>"+data[i][0]+"</td><td class='name'>"+data[i][1]+"</td><td><button class='edit'></td></tr>");
                             }
 
                             //Add listeners to edit buttons
@@ -58,6 +59,6 @@
 
         </div>
 
-        <?php //include($_SERVER['DOCUMENT_ROOT']."scripts/footer.php"); ?>
+        <?php include($_SERVER['DOCUMENT_ROOT']."scripts/footer.php"); ?>
     </body>
 </html>
