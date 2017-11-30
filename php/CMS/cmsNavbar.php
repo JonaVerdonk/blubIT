1117c3ef1be378fdc6186a2e44ee5fb7243952f9
JonaVerdonk <32864379+JonaVerdonk@users.noreply.github.com>
2017-11-30 13:37:05 +0100

    Started on edit button for Navbar

--- php/CMS/cmsNavbar.php
+++ php/CMS/cmsNavbar.php
@@ -8,7 +8,7 @@
     </head>
     <body>
 
-        <?php include($_SERVER['DOCUMENT_ROOT']."scripts/header.php"); ?>
+        <?php //include($_SERVER['DOCUMENT_ROOT']."scripts/header.php"); ?>
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109575524-1"></script>
@@ -21,16 +21,16 @@
         </script>
 
         <div id="pageContent">
-            
-            
+
+
             <table id="navbarTable"></table>
-            
+
             <div id="test"></div>
-            
+
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
             <script>
                 $(document).ready(function() {
-                    
+
                     $.ajax({
                         url: "../../scripts/executeQuery.php",
                         type: "POST",
@@ -45,15 +45,19 @@
                             var data = $.parseJSON(json);
                             $("#navbarTable").append("<tr><th>Order</th><th>URL</th><th>Name</th></tr>");
                             for (i = 0; i < data.length; ++ i) {
-                                $("#navbarTable").append("<tr><td>"+data[i][2]+"</td><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td><td></td></tr>");
+                                $("#navbarTable").append("<tr><td class='position'>"+data[i][2]+"</td><td class='url'>"+data[i][0]+"</td><td class='name'>"+data[i][1]+"</td><td><button class='edit' value='edit'></td></tr>");
                             }
+
+                            //Add listeners to edit buttons
+                            var buttons[] = $(".edit");
+                            alert(buttons.length);
                         }
                       });
                 });
             </script>
-            
+
         </div>
 
-        <?php include($_SERVER['DOCUMENT_ROOT']."scripts/footer.php"); ?>
+        <?php //include($_SERVER['DOCUMENT_ROOT']."scripts/footer.php"); ?>
     </body>
 </html>
