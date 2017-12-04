18a5f9a1293504b48f781276c3924a677cf895cd
Sander <sandert98@gmail.com>
2017-12-04 14:42:06 +0100

    users aangemaakt

--- scripts/logout.php
+++ scripts/logout.php
@@ -2,6 +2,7 @@
 print "voor logout \n";
 $_SESSION['user'] = 0;
 $_SESSION['role'] = 'r';
-$_SESSION['logged_in'] = FALSE;
+$_SESSION['logged_in'] = 0;
 print "Na logout";
+header("location: /php/login.php");
  ?>
