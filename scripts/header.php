56f000a3291b57362493569b4ced37c83fb1953c
JonaVerdonk <32864379+JonaVerdonk@users.noreply.github.com>
2017-12-06 13:29:56 +0100

    This should be the correct header

--- scripts/header.php
+++ scripts/header.php
@@ -14,7 +14,7 @@
     }
 
     if (!isset($_SESSION["logged_in"])) {
-        $_SESSION["logged_in"] = false;
+        $_SESSION["logged_in"] = 0;
     }
 
     print('<script type="text/javascript" src="/js/jquery-3.2.1.js"></script>');
@@ -25,7 +25,6 @@
     $links = executeSQL("SELECT * FROM Navbar ORDER BY position");
 
     //Print the header tag with the header-img
-    print_r($_SESSION);
     print('<header>');
     print('<img id="headerImg" src="/images/headerlogo.PNG" alt="logo">');
     //Print the navbar
@@ -57,10 +56,8 @@
         } else if (strpos($name, "log in") !== false || strpos($name, "Log in") !== false || strpos($name, "Login") !== false) {
             if ($_SESSION["logged_in"] == 1) {
                 print('<li '.$id.'><a class="navbarLink" href="/scripts/logout.php">Log uit</a></li>');
-                print($_SESSION["logged_in"] . " " . $_SESSION["user"]);
             } else {
                 print('<li '.$id.'><a class="navbarLink" href="'.$url.'">'.$name.'</a></li>');
-                print($_SESSION["logged_in"] . " " . $_SESSION["user"]);
             }
         } else {
             print('<li '.$id.'><a class="navbarLink" href="'.$url.'">'.$name.'</a></li>');
