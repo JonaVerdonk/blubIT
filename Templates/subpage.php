92d95457a0004166fd155b5168a9ccaf3df95be3
LordPinch <32864412+LordPinch@users.noreply.github.com>
2017-12-31 01:30:51 +0100

    Been buzy fixing subpage cms contol

--- Templates/subpage.php
+++ Templates/subpage.php
@@ -2,7 +2,14 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Template</title>
+    <title>
+      <?php
+      include_once("../../scripts/databaseConnection.php");
+      $filename = basename(__FILE__, '.php');
+      $Result = executeSQL("SELECT title FROM Subpages WHERE url = $filename",2);
+      echo $Result[0][0];
+      ?>
+    </title>
     <link rel="stylesheet" type="text/css" href="../../css/style.css">
   </head>
   <body>
@@ -15,8 +22,6 @@
 
         gtag('config', 'UA-109575524-1');
     </script>
-
-      <?php include_once("../../scripts/databaseConnection.php") ?>
       <?php include("../../scripts/header.php"); ?>
       <?php include("../../scripts/content.php"); ?>
       <?php include("../../scripts/footer.php"); ?>
